4688ab152d0902cbfdec186c7c41264ad52deae1
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2023-06-10 22:41:10 +0330

    Remove custom swagger settings

--- main.go
+++ main.go
@@ -6,14 +6,9 @@ import (
 	"github.com/gofiber/swagger"
 )
 
-// @title Fiber Example API
+// @title WorkerEngine API
 // @version 1.0
-// @description This is a sample swagger for Fiber
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email fiber@swagger.io
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @description WorkerEngine is a sandbox API to execute TSF based software.
 // @host localhost:8080
 // @BasePath /
 func main() {
@@ -23,21 +18,6 @@ func main() {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Get("/swagger/*", swagger.HandlerDefault) // default
-
-	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
-		URL:         "http://example.com/doc.json",
-		DeepLinking: false,
-		// Expand ("list") or Collapse ("none") tag groups by default
-		DocExpansion: "none",
-		// Prefill OAuth ClientId on Authorize popup
-		OAuth: &swagger.OAuthConfig{
-			AppName:  "OAuth Provider",
-			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
-		},
-		// Ability to change OAuth2 redirect uri location
-		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
-	}))
-
-	app.Listen(":3000")
+	app.Get("/docs/*", swagger.HandlerDefault)
+	app.Listen(":8080")
 }
