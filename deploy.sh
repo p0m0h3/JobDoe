c14b295b43379e5b7d9a43d49eb593163a41c094
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2023-06-24 23:42:21 +0330

    use /opt for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 go build && 
-swag init &&
-rsync -azP $(pwd)/env $1:~/env &&
-rsync -azP --delete $(pwd)/tools/*.toml $1:~/tools/ &&
-rsync -azP $(pwd)/workerengine $1:~/workerengine
+swag init --pd &&
+rsync -azP $(pwd)/env $1:/opt/workerengine/env &&
+rsync -azP --delete $(pwd)/tools/*.toml $1:/opt/workerengine/tools/ &&
+rsync -azP $(pwd)/workerengine $1:/opt/workerengine/workerengine
