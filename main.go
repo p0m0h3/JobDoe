2ae8c3424e1087fb780391fa1676c6a434c38211
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2023-06-27 21:28:39 +0330

    Implement token based authorization

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"log"
 	"os"
 
@@ -8,19 +10,38 @@ import (
 	"fuzz.codes/fuzzercloud/workerengine/task"
 	"fuzz.codes/fuzzercloud/workerengine/tool"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/keyauth"
 	"github.com/gofiber/swagger"
 	"github.com/joho/godotenv"
 )
 
+var accessKeyHash [32]byte
+
+func keyValidator(c *fiber.Ctx, k string) (bool, error) {
+	hashed := sha256.Sum256([]byte(k))
+
+	if subtle.ConstantTimeCompare(accessKeyHash[:], hashed[:]) == 1 {
+		return true, nil
+	}
+	return false, keyauth.ErrMissingOrMalformedAPIKey
+}
+
 // @title WorkerEngine API
 // @version 1.0
 // @description WorkerEngine is a sandbox API to execute TSF based software.
-// @host 127.0.0.1:8080
+// @host 127.0.0.1:7001
+// securitydefinitions.apikey
 // @BasePath /
 func main() {
 	app := fiber.New()
 
 	godotenv.Load("env")
+
+	accessKeyHash = sha256.Sum256([]byte(os.Getenv("ACCESS_KEY")))
+
+	app.Use(keyauth.New(keyauth.Config{
+		Validator: keyValidator,
+	}))
 
 	app.Get("/docs/*", swagger.HandlerDefault)
 
