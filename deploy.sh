26a961edd0e2466756a83c03066cecb25b9e60f1
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2023-07-01 17:46:58 +0330

    Optimise build for deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-go build && 
 swag init --pd &&
+go build -v -ldflags='-s -w' && 
 rsync -azP $(pwd)/env $1:/opt/workerengine/env &&
 rsync -azP --delete $(pwd)/tools/*.toml $1:/opt/workerengine/tools/ &&
 rsync -azP $(pwd)/workerengine $1:/opt/workerengine/workerengine
