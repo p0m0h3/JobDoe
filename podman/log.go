9514e8449c124a1bc680beab975231072fe95b84
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2023-08-09 03:30:13 +0330

    refactor: stdout stderr time based zip

--- podman/log.go
+++ podman/log.go
@@ -1,43 +1,19 @@
 package podman
 
 import (
-	"errors"
-
-	"github.com/containers/podman/v4/libpod/define"
 	"github.com/containers/podman/v4/pkg/bindings/containers"
 )
 
 func GetContainerLog(id string, stderr bool, output chan string) error {
-	con, err := InspectContainer(id)
+	False := false
+	True := true
+	err := containers.Logs(Connection, id, &containers.LogOptions{
+		Follow: &False,
+		Stderr: &stderr,
+		Stdout: &True,
+	}, output, output)
 	if err != nil {
 		return err
-	}
-
-	status, err := define.StringToContainerStatus(con.State.Status)
-	if err != nil {
-		return err
-	}
-
-	if status == define.ContainerStateStopped {
-		return errors.New("container is not stopped")
-	}
-
-	follow := false
-	err = containers.Logs(Connection, id, &containers.LogOptions{
-		Follow: &follow,
-	}, output, nil)
-	if err != nil {
-		return err
-	}
-
-	if stderr {
-		err = containers.Logs(Connection, id, &containers.LogOptions{
-			Follow: &follow,
-			Stderr: &stderr,
-		}, nil, output)
-		if err != nil {
-			return err
-		}
 	}
 
 	return nil
@@ -45,20 +21,9 @@ func GetContainerLog(id string, stderr bool, output chan string) error {
 
 func StreamContainerLog(id string, stderr bool, output chan string) error {
 	True := true
-	err := containers.Logs(Connection, id, &containers.LogOptions{
-		Follow: &True,
-	}, output, nil)
-	if err != nil {
-		return err
-	}
-
-	err = containers.Logs(Connection, id, &containers.LogOptions{
+	return containers.Logs(Connection, id, &containers.LogOptions{
 		Follow: &True,
 		Stderr: &stderr,
-	}, nil, output)
-	if err != nil {
-		return err
-	}
-
-	return nil
+		Stdout: &True,
+	}, output, output)
 }
