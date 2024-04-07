5596b562815125347a204d84e59cb86e26ba0d5a
Pooria Mokhtari <pooria@fuzzer.cloud>
2024-04-07 18:39:52 -0400

    Remove unused read tools function

--- state/tool.go
+++ state/tool.go
@@ -1,38 +1,5 @@
 package state
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
-	"git.fuzz.codes/fuzzercloud/tsf"
-)
+import "git.fuzz.codes/fuzzercloud/tsf"
 
 var Tools map[string]*tsf.Spec
-
-func ReadTools() error {
-	entries, err := os.ReadDir(os.Getenv("TOOLS_DIRECTORY"))
-	if err != nil {
-		return err
-	}
-
-	for _, toolFile := range entries {
-		if strings.HasSuffix(toolFile.Name(), ".toml") {
-			data, err := os.ReadFile(os.Getenv("TOOLS_DIRECTORY") + "/" + toolFile.Name())
-			if err != nil {
-				log.Println(fmt.Errorf("%v (Error reading file %s)", err, toolFile.Name()))
-			}
-
-			tool, err := tsf.Parse(data)
-			if err != nil {
-				log.Println(fmt.Errorf("%v (Error parsing TSF file %s)", err, toolFile.Name()))
-			}
-			if err == nil {
-				Tools[tool.Header.ID] = &tool
-			}
-		}
-	}
-
-	return nil
-}
