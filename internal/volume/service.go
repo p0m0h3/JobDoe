7ed66d84c23ce44be845901135835daba7c6a64f
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2025-11-19 14:52:15 +0330

    Implement volumes service

--- internal/volume/service.go
+++ internal/volume/service.go
@@ -2,6 +2,7 @@ package volume
 
 import (
 	"context"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 )
@@ -18,4 +19,24 @@ func NewService(s3 *minio.Client) VolumeService {
 
 func (s VolumeService) CreateVolume(name string) error {
 	return s.S3Client.MakeBucket(context.Background(), name, minio.MakeBucketOptions{})
+}
+
+func (s VolumeService) DeleteVolume(name string) error {
+	return s.S3Client.RemoveBucket(context.Background(), name)
+}
+
+func (s VolumeService) GetUploadURL(bucket string, name string) (string, error) {
+	url, err := s.S3Client.PresignedPutObject(context.Background(), bucket, name, 10*time.Minute)
+	if err != nil {
+		return "", err
+	}
+	return url.String(), nil
+}
+
+func (s VolumeService) GetDownloadURL(bucket string, name string) (string, error) {
+	url, err := s.S3Client.PresignedGetObject(context.Background(), bucket, name, 10*time.Minute, nil)
+	if err != nil {
+		return "", err
+	}
+	return url.String(), nil
 }
